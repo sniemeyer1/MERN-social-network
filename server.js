0552623b7437572db0a6a5ec9399420b0cbc94be
Savannah Niemeyer <savannahniemeyer@gmail.com>
2020-04-15 13:23:35 -0500

    routed files with express

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ connectDB();
 //create single enpoint to test out
 //take get request to /, callback is req, res, sends data to browser
 app.get('/', (req, res) => res.send('API running'));
+
+//define routes, put in endpoint
+//can make request to all the endpoints
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 //listen on port via the variable PORT
 //for heroku, OR default port locally
 const PORT = process.env.PORT || 5000;
