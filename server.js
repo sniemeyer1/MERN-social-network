b02d87dae9b7b078b641d9ca7c09efb653077749
Savannah Niemeyer <savannahniemeyer@gmail.com>
2020-04-15 01:45:17 -0500

    connected to mongoDB with mongoose

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 //bring in express
 const express = require('express');
+
+//bring in connectdb
+const connectDB = require('./config/db.js');
 //initialize app viable with express
 const app = express();
+//conecct db
+connectDB();
 //create single enpoint to test out
 //take get request to /, callback is req, res, sends data to browser
 app.get('/', (req, res) => res.send('API running'));
