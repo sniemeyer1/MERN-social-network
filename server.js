f84f36b5044a36db25ba0c3a8a2f9c2b8ff175b8
Savannah Niemeyer <savannahniemeyer@gmail.com>
2020-04-14 23:55:49 -0500

    initial commit, express server set up

---
+++ server.js
@@ -0,0 +1,12 @@
+//bring in express
+const express = require('express');
+//initialize app viable with express
+const app = express();
+//create single enpoint to test out
+//take get request to /, callback is req, res, sends data to browser
+app.get('/', (req, res) => res.send('API running'));
+//listen on port via the variable PORT
+//for heroku, OR default port locally
+const PORT = process.env.PORT || 5000;
+//pass in PORT and call back message that server is working
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
